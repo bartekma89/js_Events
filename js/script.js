b13ddd35b42ed1ab30ac48ad4f8c2da211e5b3de
Bartek <bartekma89@gmail.com>
2017-11-21 19:11:04 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+var list = document.getElementById('js-list');
+var addBtn = document.querySelector('#js-addBtn');
+var removeBtn = document.querySelector('#js-removeBtn');
+var resetBtn = document.querySelector('#js-resetBtn');
+
+addBtn.addEventListener('click', function () {
+	var listLength = document.getElementsByTagName('li').length;
+	var newElementList = document.createElement('li');
+	var currentNumberItem = null;
+	
+	if (listLength > 1) {
+		currentNumberItem = document.getElementsByTagName('li')[document.getElementsByTagName('li').length - 1].textContent.replace('Item ', '');
+	}
+	if (listLength > 1 && listLength != currentNumberItem) {
+		addElement(Number(currentNumberItem));
+	} else {
+		addElement(listLength);
+	}
+
+	function addElement(numberItem) {
+		newElementList.innerHTML = "Item " + (++numberItem);
+		list.appendChild(newElementList);
+	};
+
+}, false);
+
+removeBtn.addEventListener('click', function () {
+	var elementNumber = 0;
+	var elementToRemove = document.getElementsByTagName('li')[elementNumber++];
+	elementToRemove.parentNode.removeChild(elementToRemove);
+
+}, false);
+
+resetBtn.addEventListener('click', function () {
+	var listLength = document.getElementsByTagName('li').length;
+	var elementToRemove = document.querySelectorAll('li');
+
+	if (listLength > 0) {
+		for (var elementNumber = 0; elementNumber < listLength; elementNumber++) {
+			elementToRemove[elementNumber].parentNode.removeChild(elementToRemove[elementNumber]);
+		}
+	}
+	
+}, false);
